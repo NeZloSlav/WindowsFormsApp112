1a8b491fb1eef02259c5374698b679050731c28e
Viacheslav_Nakhodnov <nahodnoff.vyacheslaw@yandex.ru>
2022-12-26 13:52:29 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp112/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿using System.Drawing;
+
+namespace WindowsFormsApp112
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbxLogin = new System.Windows.Forms.TextBox();
+            this.tbxPassword = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblCapcha = new System.Windows.Forms.Label();
+            this.tbxCapcha = new System.Windows.Forms.TextBox();
+            this.btnEnter = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbxLogin
+            // 
+            this.tbxLogin.BackColor = System.Drawing.Color.White;
+            this.tbxLogin.Location = new System.Drawing.Point(110, 47);
+            this.tbxLogin.Name = "tbxLogin";
+            this.tbxLogin.Size = new System.Drawing.Size(100, 20);
+            this.tbxLogin.TabIndex = 0;
+            // 
+            // tbxPassword
+            // 
+            this.tbxPassword.Location = new System.Drawing.Point(110, 97);
+            this.tbxPassword.Name = "tbxPassword";
+            this.tbxPassword.Size = new System.Drawing.Size(100, 20);
+            this.tbxPassword.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(46, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Логин";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(46, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Пароль";
+            // 
+            // lblCapcha
+            // 
+            this.lblCapcha.AutoSize = true;
+            this.lblCapcha.Font = new System.Drawing.Font("Ravie", 12F, ((System.Drawing.FontStyle)(((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic) 
+                | System.Drawing.FontStyle.Strikeout))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCapcha.Location = new System.Drawing.Point(45, 141);
+            this.lblCapcha.Name = "lblCapcha";
+            this.lblCapcha.Size = new System.Drawing.Size(23, 22);
+            this.lblCapcha.TabIndex = 4;
+            this.lblCapcha.Text = "0";
+            // 
+            // tbxCapcha
+            // 
+            this.tbxCapcha.Location = new System.Drawing.Point(49, 166);
+            this.tbxCapcha.Name = "tbxCapcha";
+            this.tbxCapcha.Size = new System.Drawing.Size(100, 20);
+            this.tbxCapcha.TabIndex = 5;
+            // 
+            // btnEnter
+            // 
+            this.btnEnter.Location = new System.Drawing.Point(110, 210);
+            this.btnEnter.Name = "btnEnter";
+            this.btnEnter.Size = new System.Drawing.Size(75, 23);
+            this.btnEnter.TabIndex = 6;
+            this.btnEnter.Text = "Войти";
+            this.btnEnter.UseVisualStyleBackColor = true;
+            this.btnEnter.Click += new System.EventHandler(this.btnEnter_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(173, 163);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(73, 23);
+            this.btnUpdate.TabIndex = 7;
+            this.btnUpdate.Text = "Обновить";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(275, 245);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnEnter);
+            this.Controls.Add(this.tbxCapcha);
+            this.Controls.Add(this.lblCapcha);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbxPassword);
+            this.Controls.Add(this.tbxLogin);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbxLogin;
+        private System.Windows.Forms.TextBox tbxPassword;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblCapcha;
+        private System.Windows.Forms.TextBox tbxCapcha;
+        private System.Windows.Forms.Button btnEnter;
+        private System.Windows.Forms.Button btnUpdate;
+    }
+}
+
---
+++ WindowsFormsApp112/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.OleDb;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp112
+{
+    public partial class Form1 : Form
+    {
+        string connectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=|DataDirectory|\Database2.mdb";
+        public Form1()
+        {
+            InitializeComponent();
+
+            lblCapcha.Text = GetCapcha();
+            
+        }
+
+        private void btnEnter_Click(object sender, EventArgs e)
+        {
+            using (OleDbConnection connection = new OleDbConnection(connectionString))
+            {
+                connection.Open();
+
+                OleDbCommand command = new OleDbCommand
+                {
+                    CommandText = "SELECT * FROM [Таблица1] WHERE [Логин] = @login AND [Пароль] = @password",
+                    Connection = connection
+                };
+
+                OleDbParameter[] parameters =
+                {
+                    new OleDbParameter("@login", tbxLogin.Text),
+                    new OleDbParameter("password", tbxPassword.Text)
+                };
+
+                command.Parameters.AddRange(parameters);
+                OleDbDataReader reader = command.ExecuteReader();
+                if (reader.HasRows && (lblCapcha.Text == tbxCapcha.Text))
+                {
+                    MessageBox.Show("Добро пожаловать");
+                }
+                else
+                {
+                    MessageBox.Show("Такого пользователя не существует");
+                }
+                
+            }
+        }
+
+        private string GetCapcha()
+        {
+            char[] charArray = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM".ToCharArray();
+            char[] numArray = "1234567890".ToCharArray();
+
+            string text = "";
+            Random rnd = new Random();
+            for (int i = 0; i < 5; i++)
+            {
+                int a = rnd.Next(1, 3);
+                if (a == 1)
+                {
+                    text += charArray[rnd.Next(0, charArray.Length - 1)];
+                }
+                else
+                {
+                    text += numArray[rnd.Next(0, numArray.Length - 1)];
+                }
+            }
+
+            return text;
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            lblCapcha.Text = GetCapcha();
+        }
+    }
+}
